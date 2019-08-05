bbae3ee87cb09baab79ab388fb5740e2c11a03c3
Hom Lamsal <hom.geek005@gmail.com>
2019-08-05 14:24:22 -0500

    comitt

--- Helloworld/Helloworld/Program.cs
+++ Helloworld/Helloworld/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿
+/*
+Project:    Lab 2 Calculate Batting Average
+Date:        to/da/y
+Author:     You the big new programmer
+Notes:      This program reads in a baseball player's name,
+            number of hits and number of at bats.
+            The player's batting average is calculated and displayed
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +21,70 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Matrix Has you...");
+            //variable declaration
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAvrage;
+
+
+            //prompt for name recive 
+            Console.WriteLine("~this programe calculate abaseball player's batting avrage~");
+            Console.WriteLine("<------------>");
+            Console.WriteLine("");//we will learn other ways to do this on console programs
+            Console.WriteLine("Enter the player's name ");
+            strPlayerName = Console.ReadLine();
+            Console.WriteLine(strPlayerName);
             Console.ReadLine();
-            //hgjgggj comment 
+
+            //prompt for name and recive
+            Console.WriteLine("~This programe calcitates a baseball player's batting average~");
+            Console.WriteLine("<----------------------------->");
+            Console.WriteLine("");//we will learn other ways to do this in console programs
+            Console.WriteLine("Enter the playes's name: ");
+            strPlayerName = Console.ReadLine();
+
+            //prompt for name and receive 
+            Console.WriteLine("~This programe calcitates a baseball player's batting average~");
+            Console.WriteLine("<----------------------------->");
+            Console.WriteLine("");//we will learn other ways to do this in console programs
+            Console.WriteLine("Enter the playes's name: ");
+            strPlayerName = Console.ReadLine();
+
+            //prompt for hits
+            Console.WriteLine("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+            //prompt for hits, walks and at bats
+            Console.WriteLine("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the player's # of at bats: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+
+            //prompt for hits, walks and at bats
+            Console.WriteLine("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the player's # of at bats: ");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+           
+            //calculation
+
+            dblBattingAvrage = nHits / nAtBats;
+
+
+            Console.WriteLine("Enter the player's # of at bats: ");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            dblBattingAvrage = nHits / nAtBats;
+
+            Console.WriteLine(strPlayerName + "'s batting avarege is " + dblBattingAvrage);
+            Console.ReadLine();
+
+
 
         }
-    }
+        }
 }
---
+++ Lab 3 BMI Calculator/Lab 3 BMI Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_BMI_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare variables
+            double dblHeight;
+            double dblWeight;
+            double dblBMI;
+            //user input 
+            Console.WriteLine("Enter your height in inches: ");
+            dblHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your Weight in pounds: ");
+            dblWeight = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            //calcultion 
+            dblBMI = (dblWeight * 703) / (dblHeight * dblHeight);
+
+            //issue result and message 
+            Console.WriteLine("Your BMI is " + dblBMI);
+            if(dblBMI < 18.5)
+            {
+                Console.WriteLine("BMI < 18.5 - UnderWeight");
+
+            }   
+            else if(dblBMI <= 24.9)
+            {
+                Console.WriteLine("BMI between 18.5 and 24.9 - Normal");
+
+            }
+            else if (dblBMI <= 29.9)
+            {
+                Console.WriteLine("BMI between 25 and 29.9 - Overweight");
+
+            }
+
+            else
+            {
+                Console.WriteLine("BMI >= 30 - Obese");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
